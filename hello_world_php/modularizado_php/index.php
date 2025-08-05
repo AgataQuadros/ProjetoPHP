addb1e1c7a140e49bae26cad1de184c759ddb65f
AgataQuadros <quadrosagata@gmail>
2025-08-05 19:25:04 -0300

    Meu Primeiro PHP Modularizado

--- hello_world_php/modularizado_php/index.php
+++ hello_world_php/modularizado_php/index.php
@@ -7,27 +7,17 @@
     <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
-    <h1>Bem-Vindo ao PHP</h1>
+    <h1>Envie sua Mensagem</h1>
 
-    <form method="GET" action="">
+    <form method="POST" action="processa.php">
+
         <label>Digite sua mensagem:</label><br>
 
         <input type="text" name="mensagem" require>
 
         <button type="submit">Enviar</button>
+        
     </form>
-
-    <p>
-        <?php
-        if (isset($_GET['mensagem'])) {
-            $mensagem = htmlspecialchars($_GET['mensagem']);
-
-            echo "Você digitou: <strong>$mensagem</strong>";
-        } else {
-            echo "Olá, mundo! Este é meu primeiro código em PHP";
-        }
-        ?>
-    </p>
     
 </body>
 </html>

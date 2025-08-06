6a58d244bf7e170948f5a094a4e31c0626f27a7e
AgataQuadros <quadrosagata@gmail>
2025-08-05 21:38:43 -0300

    Página principal e Validação

--- Atividades/Atividade001_operadores_aritimeticos/index.php
+++ Atividades/Atividade001_operadores_aritimeticos/index.php
@@ -7,6 +7,28 @@
     <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
+    <header>
+        <h1>Calculadora</h1>
+    </header>
+
+    <div class="caixa">
+        <form action="processa.php" method="POST" id="formulario">
+
+            <label for="val-um">Digite o primeiro número</label>
+            <input type="number" id="val-um">
+
+            <label for="val-dois">Digite o segundo número</label>
+            <input type="number" id="val-dois">
+
+            <label for="selecao">Selecione uma operação</label>
+            <select name="selecao" id="selecao">
+                <option value="soma">Soma (+)</option>
+                <option value="subtracao">Subtração (-)</option>
+                <option value="multiplicacao">Multiplicação (x)</option>
+                <option value="divisao">Divisão (÷)</option>
+            </select>
+        </form>
+    </div>
 
 <script src="js/script.js"></script>
     
--- Atividades/Atividade001_operadores_aritimeticos/js/script.js
+++ Atividades/Atividade001_operadores_aritimeticos/js/script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
+    const form = document.getElementById('formulario')
+
+    form.addEventListener('submit', function(){
+
+        const um = document.getElementById('val-um')
+        const dois = document.getElementById('val-dois')
+
+        if(um === "" || dois === ""){
+            alert("Não se faz conta sem um número, adicione um valor e tente novamente");
+            return;
+        }
+    })
 })
